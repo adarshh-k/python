92a0dc224443a4b09608db83d7d1444058dc5adf
adarshh-k <adarshsuresh2727@gmail.com>
2026-07-26 00:43:32 +0530

    Add files via upload

---
+++ assign4.py
@@ -0,0 +1,64 @@
+# #1. check whether apple is present
+# t=('apple','pineapple','mango','kiwi')
+# if 'apple' in t:
+#     print("apple found")
+# else:
+#     print("apple not found")
+
+
+# #2. pass if first mark>40
+# t=(30,40,50,60,70)
+# if t[0]>40:
+#     print("passed")
+# else:
+#     print("failed!")
+
+
+# #3.adding element in tuple
+# t=(11,22,33,44,55)
+# li=list(t)
+# li.append(100)
+# print(tuple(li))
+
+
+# #4. replace in tuple
+# t=('python','java','c++')
+# li=list(t)
+# li[1]="sql"
+# print(tuple(li))
+
+
+# #5. joining two tuples to one
+# t1=(1,3,5,7,9)
+# t2=(0,2,4,6,8)
+# t3=t1+t2
+# print(t3)
+
+
+# #6. same if first and last name are same
+# t1=("adhu","achu","adhu")
+# if t1[0]==t1[2]:
+#     print("same")
+# else:
+#     print("different")
+
+
+# #7. print last element of 2nd  tuple
+# t1=((1,2,3,4),(5,6,7,8))
+# print(t1[1][3])
+
+
+# #8. to get last item in tuple
+# t=('jee','efef','fe','weedw','wwdwd')
+# print(t[-1])
+
+
+# #9. finding poition in tuple
+# t=(1,2,3,4,5,6,8)
+# print(t.index(3))
+
+
+# #10. converting tuple into list
+# t=(10,20,30,40,50)
+# li=list(t)
+# print(li)
